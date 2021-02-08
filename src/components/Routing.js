073aba672adea7696f36c70ea02ac436d25a816e
youngchingjui <young.chingjui@gmail.com>
2021-02-08 12:27:22 +0800

    init new timeline feature

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import useSubdomain from '../hooks/useSubdomain'
-import Auth from '../pages/Auth'
-import Dashboard from '../pages/Dashboard'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import useSubdomain from "../hooks/useSubdomain";
+import Auth from "../pages/Auth";
+import Dashboard from "../pages/Dashboard";
+import Timeline from "../pages/Timeline";
 
 const Routing = () => {
-  const { subdomain } = useSubdomain()
+  const { subdomain } = useSubdomain();
   return (
     <Switch>
-      {subdomain === 'dashboard' && (
-        <Route exact path="/">
-          <Dashboard />
-        </Route>
+      {subdomain === "dashboard" && (
+        <>
+          <Route exact path="/timeline">
+            <Timeline />
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+        </>
       )}
-      {subdomain === 'auth' && (
+      {subdomain === "auth" && (
         <Route exact path="/">
           <Auth />
         </Route>
       )}
     </Switch>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
---
+++ src/pages/Timeline/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timeline = () => {
+  return <h1>Timeline</h1>;
+};
+
+export default Timeline;
