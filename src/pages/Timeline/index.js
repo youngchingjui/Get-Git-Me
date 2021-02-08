3136b392f2b95faf837831b9afb492cd33c9e661
youngchingjui <young.chingjui@gmail.com>
2021-02-08 12:30:14 +0800

    lint

--- src/pages/Timeline/index.js
+++ src/pages/Timeline/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const Timeline = () => {
-  return <h1>Timeline</h1>;
-};
+  return <h1>Timeline</h1>
+}
 
-export default Timeline;
+export default Timeline
